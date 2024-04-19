9e0f1f08ab72b679128b147f947d7fa6c5382c24
mcfrancho1 <mcfranchostudios@gmail.com>
2024-04-19 01:04:10 -0300

    moved after_setup to deploy function

--- R/MAST21_2022.R
+++ R/MAST21_2022.R
@@ -191,26 +191,79 @@ after_setup <- function(page_type = "record_midi_page",
 #'
 #' @examples
 
-deploy_MAST21_2024 <- function(
+deploy_MAST21_Old_2024 <- function(
     app_name = paste("UPEI ", format(Sys.Date(), "%Y"), " Study"),
     language,
     musicassessr_state = "test",
     absolute_url = "https://musicog.ca/",
-    data_collection_method = "audio",
-    get_p_id = FALSE,
     final_qualtrics_url = 'https://upeiairs.qualtrics.com/jfe/form/SV_5vDAjJhxLqZw7Km?participant=',
     opening_and_final_image = "https://img.freepik.com/free-vector/headphone-concept-illustration_114360-2132.jpg?w=826&t=st=1708543460~exp=1708544060~hmac=7a9cba8f7104f82422a55f30f5120598e4ce64dd222a1247c8da79825dddacb6"
 ) {
 
 
+    psychTestR::make_test(
+      psychTestR::join(
+        psychTestR::new_timeline(
+          psychTestR::join(
 
-  after_setup(
-    get_p_id = get_p_id,
-    absolute_url = absolute_url,
-    data_collection_method = data_collection_method,
-    final_qualtrics_url = final_qualtrics_url,
-    language = language
-  )
+
+
+            musicassessr::musicassessr_init(),
+
+            upei_intro(musicassessr_state),
+
+            musicassessr::setup_pages(input = "microphone", absolute_url = absolute_url),
+
+
+            say_pd(dinosaur_instructions = "Please press the “record” button and read the sentence below out loud: ",
+                   body_instructions = 'Although the next short test involves singing,
+                                      we would like to start off by asking you to read out loud four short sentences all beginning with the phrase
+                                      "The hungry purple dinosaur".  The sentences may sound silly, but together,
+                                      they cover all the sounds of the English language.'),
+
+            psychTestR::elt_save_results_to_disk(complete = FALSE),
+
+
+            musicassessr::long_tone_trials(num_items = 6)
+
+          ),
+
+          dict  = musicassessr::dict(NULL),
+          default_lang = language
+
+        ), # end timeline (it's not needed from here onwards, and the SAA is embedded in UPEI_extra_questions, so to avoid nesting)
+
+
+
+        mast_21(mast_inst = "You will now have another test of short singing examples.
+                    There are 2 sets of 21 questions. The first 20 are very short.
+                    Like the previous test, you will hear a melody and be asked to imitate.
+                    Unlike the previous test, in which you sang along with the example, now you will listen and then sing: you will hear the example and then sing the imitation after it.
+                    You will be asked to sing each of the two sets of 21 examples on a different syllable:  one set on /da/ (“Daah”) and the other on /du/ (“Dooo”).
+                    The instructions before each set of 21 examples will let you know which syllable to use.
+                    You will also be asked to sing “Happy birthday” on four occasions."),
+
+        psyquest::GMS(),
+
+        UPEI_extra_questions(with_compensation_question = FALSE),
+
+        musicassessr::sing_happy_birthday_page(feedback = FALSE, label = "sing_hbd4", text = "Please sing Happy Birthday."),
+
+
+        psychTestR::elt_save_results_to_disk(complete = TRUE),
+
+        psychTestR::reactive_page(function(state, ... ) {
+          p_id <- psychTestR::get_global('p_id', state)
+          url <- paste0(final_qualtrics_url, p_id)
+          psychTestR::final_page(shiny::tags$div(shiny::tags$p("Please click on the following link to go to the final test of this session: ",
+                                                               shiny::tags$a(" click here", href = url, target = "_blank"), ".")))
+        })
+
+      )
+      ,
+      opt = upei_test_options(musicassessr_state)
+    )
+
 
 }
 
